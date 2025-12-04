71fc96dd51c1564ab93be2c1c8cf59c23a569e5a
Rutika Parekar <rutikaparekar@gmail.com>
2025-12-04 09:52:21 +0530

    Add Searching code

---
+++ Searching/linarSearch.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+
+public class linarSearch {
+   
+    public static void linearSearch(int arr[],int key){
+       
+        for(int i=0;i<arr.length;i++){
+            if(key==arr[i]){
+                System.err.println(arr[i]+"  found at index: "+i);
+               return;
+            }
+        }
+         System.out.println("not found");
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[]={11,2,3,4,5};
+         int key=4;
+        linearSearch(arr,key);
+    }
+}
