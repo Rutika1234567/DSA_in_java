3a1412fb89ea15c7f42e08af3bfc949346a646da
Rutika Parekar <rutikaparekar@gmail.com>
2025-12-22 12:16:52 +0530

    Adding multidimention codes fo my practice

---
+++ ArrayList/multidimention2D.java
@@ -0,0 +1,33 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class multidimention2D {
+    
+    public static void main(String[] args) {
+    
+     ArrayList<ArrayList<Integer>> mainlist=new ArrayList<>();
+       ArrayList<Integer> list1=new ArrayList<>();
+         list1.add(1);
+         list1.add(2);
+        mainlist.add(list1);
+
+
+     ArrayList<Integer> list2=new ArrayList<>();
+          list2.add(3);
+         list2.add(4);
+  
+      mainlist.add(list2);
+
+      System.out.println(mainlist);
+
+      for(int i=0;i<mainlist.size();i++){
+        ArrayList<Integer> currlist=mainlist.get(i);
+        for(int j=0;j<currlist.size();j++){
+           System.out.print(currlist.get(j)+" ");
+        }
+        System.out.println();
+      }
+      
+    }
+
+}
