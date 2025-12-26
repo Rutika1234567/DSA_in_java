80eed89e238d4ccd672a5de11dc843bf668eec4d
Rutika Parekar <rutikaparekar@gmail.com>
2025-12-26 20:38:28 +0530

    Adding reverse program

---
+++ ArrayList/reverse.java
@@ -0,0 +1,20 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class reverse {
+   
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        list.add(34);
+        list.add(11);
+        list.add(4);
+        for(int i=list.size()-1;i>=0;i--){
+            System.out.print(list.get(i)+" ");
+        }
+        
+    }
+
+
+}
