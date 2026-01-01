675452f2674da46fef49217f5eeedfed5876a870
Rutika Parekar <rutikaparekar@gmail.com>
2026-01-01 16:47:49 +0530

    Implementing merge sort using Linked list

---
+++ LinkedList/SinglyLinkedList/mergesort_ll.java
@@ -0,0 +1,117 @@
+package LinkedList.SinglyLinkedList;
+
+public class mergesort_ll {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    // Find middle
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Merge two sorted lists
+    private Node merge(Node head1, Node head2) {
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return dummy.next;
+    }
+
+    // Merge Sort
+    public Node mergesort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node mid = getMid(head);
+        Node rightHead = mid.next;
+        mid.next = null;
+
+        Node left = mergesort(head);
+        Node right = mergesort(rightHead);
+
+        return merge(left, right);
+    }
+
+    // Add node at end
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Print list
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        mergesort_ll ll = new mergesort_ll();
+
+        ll.addLast(5);
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(0);
+        ll.addLast(1);
+
+        System.out.print("Before Sort: ");
+        ll.printList();
+
+        ll.head = ll.mergesort(ll.head);
+
+        System.out.print("After Sort: ");
+        ll.printList();
+    }
+}
