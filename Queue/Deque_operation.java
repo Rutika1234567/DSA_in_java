a76f98727c72c9b98685bef2725d6a547c9c6810
Rutika Parekar <rutikaparekar@gmail.com>
2026-01-10 12:34:41 +0530

    Implementing Deque operations

---
+++ Queue/Deque_operation.java
@@ -0,0 +1,21 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Deque_operation {
+    public static void main(String[] args) {
+        Deque<Integer> deque=new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        System.out.println(deque);
+
+        deque.removeFirst();
+        System.out.println(deque);
+        deque.removeLast();
+        System.out.println(deque);
+        System.out.println("first "+deque.getFirst());
+        System.out.println("last "+deque.getLast());
+
+    }
+}
