49f12928ae07f227d1fbf1b73fbd3230037f71b2
Rutika Parekar <rutikaparekar@gmail.com>
2026-01-15 11:09:32 +0530

    Implemented greedy algorithm for the Activity Selection problem

---
+++ Greedy/Activity_selection.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Activity_selection {
+ 
+    public static void main(String[] args) { //O(n)
+         int start[]={1,3,0,5,8,5};
+         int end[]={2,4,6,7,9,9};
+
+        //end time basis sorted
+        
+        int maxAct=0;
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        //1st acitivity
+        maxAct=1;
+        ans.add(0);
+        int lastEnd=end[0];
+        for(int i=1;i<end.length;i++){
+            if(start[i]>=lastEnd){
+                //acitivity select
+                maxAct++;
+                ans.add(i);
+                lastEnd=end[i];
+            }
+        }
+
+        System.out.println("max acitivity= "+maxAct);
+        for(int i=0;i<ans.size();i++){
+            System.out.print("A"+ans.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
