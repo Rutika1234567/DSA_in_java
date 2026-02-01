b52a1485d7e2780f2b751735bb607d6cb31d4a3c
Rutika Parekar <rutikaparekar@gmail.com>
2026-02-01 10:45:43 +0530

    Add functionality to count total nodes in a binary tree

---
+++ Binary_Tree/count_nodes.java
@@ -0,0 +1,36 @@
+public class count_nodes {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+     public static int count(Node root){
+          if(root==null){
+            return 0;
+          }  
+         int lcount=count(root.left);
+         int rcount=count(root.right);
+         int treecount=lcount+rcount+1;
+         return treecount;     
+     }
+
+
+    public static void main(String[] args) {
+        
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.left=new Node(6);
+       System.out.println("Number of nodes: "+count(root));
+
+    }
+    
+    
+}
