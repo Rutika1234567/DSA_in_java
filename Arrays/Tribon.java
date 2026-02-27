625f6a2ae31b6c94db57c9aa225cb7aa8e506d26
Rutika Parekar <rutikaparekar@gmail.com>
2026-02-27 16:49:56 +0530

    Adding iterative Tribonacci implementation

---
+++ Arrays/Tribon.java
@@ -0,0 +1,32 @@
+class Tribon {
+
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        int first = 0;
+        int second = 1;
+        int third = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int next = first + second + third;
+            first = second;
+            second = third;
+            third = next;
+        }
+        return third;
+    }
+
+    public static void main(String[] args) {
+        Tribon sol = new Tribon();
+
+        int n = 5; // example input
+        int result = sol.tribonacci(n);
+
+        System.out.println("Tribonacci(" + n + ") = " + result);
+    }
+}
