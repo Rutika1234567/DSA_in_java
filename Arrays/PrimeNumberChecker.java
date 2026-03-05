3949319ba3b0865136221fc41fd0ca76cfc09644
Rutika Parekar <rutikaparekar@gmail.com>
2026-03-05 21:50:45 +0530

    Implement checking  numbers in the array are prime

---
+++ Arrays/PrimeNumberChecker.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Prime numbers in the array:");
+
+        for (int i = 0; i < n; i++) {
+            if (isPrime(arr[i])) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
